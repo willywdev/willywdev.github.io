16d2b1a9bdee30d3b613481b38f2195139af7ddb
willywdev <werwein.dev@gmail.com>
2023-05-17 12:39:15 +0200

    added hamburger menu + little javascript for it

---
+++ main.js
@@ -0,0 +1,11 @@
+function openNavbar() {
+    document.getElementById("menu").style.display = "flex";
+    document.getElementById("menu-open").style.display = "none";
+    document.getElementById("menu-close").style.display = "block";
+}
+
+function closeNavbar() {
+    document.getElementById("menu-close").style.display = "none";
+    document.getElementById("menu-open").style.display = "block";
+    document.getElementById("menu").style.display = "none";
+}
